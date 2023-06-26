653605189798a5b0fd82664f929cb749455e8d69
DS <mc_sd@bk.ru>
2023-06-26 16:47:47 -0300

    Selenide

---
+++ src/test/java/ru/netology/stats/BankCardTest.java
@@ -0,0 +1,13 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class BankCardTest {
+     @Test
+     void shouldTest() {
+         open("http://0.0.0.0:9999");
+
+     }
+ }
