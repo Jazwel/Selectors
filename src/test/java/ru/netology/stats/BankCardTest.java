625587c2b099533702fced052d7c418927dc9fb3
DS <mc_sd@bk.ru>
2023-06-27 10:43:52 -0300

    BCT

--- src/test/java/ru/netology/stats/BankCardTest.java
+++ src/test/java/ru/netology/stats/BankCardTest.java
@@ -9,18 +9,19 @@ import static com.codeborne.selenide.Selenide.*;
 
 class BankCardTest {
     @BeforeEach
-    void setUp(){
+    void setUp() {
         open("http://localhost:9999/");
     }
-     @Test
-     void shouldSignCorrect() {
 
-         SelenideElement form = $("[class]");
-         form.$("[data-test-id=name] input").setValue("Иванов Иван");
-         form.$("[data-test-id=phone] input").setValue("+79102222222");
-         form.$("[data-test-id=agreement]").click();
-         form.$("button").click();
-         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    @Test
+    void shouldSignCorrect() {
+
+        SelenideElement form = $("[class]");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79102222222");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
- }
+}
