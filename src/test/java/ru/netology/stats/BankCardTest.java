3b1e596cf06ae2813099766015d0464905e2ca94
DS <mc_sd@bk.ru>
2023-06-26 21:39:57 -0300

    BankCardTest

--- src/test/java/ru/netology/stats/BankCardTest.java
+++ src/test/java/ru/netology/stats/BankCardTest.java
@@ -1,16 +1,31 @@
 package ru.netology.stats;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.conditions.ExactText;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
 
 class BankCardTest {
+    @BeforeEach
+    void setUp(){
+        open("http://localhost:9999/");
+    }
      @Test
      void shouldTest() {
-         open("http://0.0.0.0:9999");
-        // SelenideElement form = $("[]")
+
+         SelenideElement form = $("[class]");
+         form.$("[data-test-id=name] input").setValue("Мойва Лох");
+         form.$("[data-test-id=phone] input").setValue("+79251111111");
+         form.$("[data-test-id=agreement]").click();
+         form.$("button").click();
+         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+
+
+
 
      }
  }
